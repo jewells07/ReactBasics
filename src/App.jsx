49a98842d1653b70e14624af43460809f7f60f83
jewells <jewellsjoshi437@gmail.com>
2020-06-26 19:15:31 +0530

    Mini Project->Greeting(with Component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import Heading from './Heading';
-import Para from './Para';
-import List from './List';
-
+import './index.css';
 
 function App() {
-    return (
-        <>
-            <Heading />
-            <Para />
-            <List />
-        </>
-    );
+    let currDate = new Date();
+    currDate = currDate.getHours();
+    let greeting = '';
+    const cssStyle = {};
+
+    if (currDate >= 1 && currDate < 12) {
+        greeting = 'Good Morning';
+        cssStyle.color = 'green';
+    }
+    else if (currDate >= 12 && currDate < 19) {
+        greeting = 'Good Afternoon';
+        cssStyle.color = 'Orange';
+    }
+    else {
+        greeting = 'Good Evening';
+        cssStyle.color = 'Black';
+    }
+    return (<>
+        <div>
+            <h1>
+                Hello Sir, <span style={cssStyle}>{greeting}</span>
+            </h1>
+        </div>
+    </>);
 }
 
 export default App;
