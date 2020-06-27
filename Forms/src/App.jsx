bb5954161209b6d5747a7411715d60e5c13b15f8
jewells <jewellsjoshi437@gmail.com>
2020-06-27 21:04:52 +0530

    Login Form

--- Forms/src/App.jsx
+++ Forms/src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
     const [fullName, setFullName] = useState({
         fname: "",
         lname: "",
+        email: "",
+        phone: "",
     });
 
     const inputEvent = (event) => {
@@ -11,21 +13,45 @@ const App = () => {
         // console.log('words ', event.target.name);
         // console.log('words ', event.target.placeholder);
 
-        const value = event.target.value;
-        const name = event.target.name;
+        // Destructuring
+        const { value, name } = event.target;
 
         // React setState pervious value
         setFullName((preValue) => {
             // console.log(preValue);
             if (name === 'fname') {
                 return {
-                    'fname': value,
-                    'lname': preValue.lname,
+                    fname: value,
+                    lname: preValue.lname,
+                    email : preValue.email,
+                    phone : preValue.phone
                 };
-            }else if (name === 'lname') {
+            }
+            
+            else if (name === 'lname') {
                 return {
-                    'fname': preValue.fname,
-                    'lname': value,
+                    fname: preValue.fname,
+                    lname: value,
+                    email : preValue.email,
+                    phone : preValue.phone
+                };
+            }
+            
+            else if (name === 'email') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email : value,
+                    phone : preValue.phone
+                };
+            }
+            
+            else if (name === 'phone') {
+                return {
+                    fname: preValue.fname,
+                    lname: preValue.lname,
+                    email : preValue.email,
+                    phone : value,
                 };
             }
 
@@ -43,8 +69,15 @@ const App = () => {
                 <form onSubmit={onSubmit}>
                     <div>
                         <h1>Hello {fullName.fname} {fullName.lname}</h1>
+                        <p>{fullName.email}</p>
+                        <p>{fullName.phone}</p>
+
                         <input type="text" placeholder="Enter Your Name" name="fname" onChange={inputEvent} value={fullName.fname} />
                         <input type="text" placeholder="Enter Your Last" name="lname" onChange={inputEvent} value={fullName.lname} />
+
+                        <input type="email" placeholder="Enter Your Email" name="email" onChange={inputEvent} value={fullName.email} autoComplete="off" />
+                        <input type="phone" placeholder="Enter Your Mobile no." name="phone" onChange={inputEvent} value={fullName.phone} />
+
                         <button type="submit">Submit</button>
                     </div>
                 </form>
