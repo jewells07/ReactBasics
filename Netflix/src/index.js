53cb52777370c64dfae0481b8734caed99c78cbd
jewells <jewellsjoshi437@gmail.com>
2020-06-27 11:58:07 +0530

    Array Map and Arrow function

--- Netflix/src/index.js
+++ Netflix/src/index.js
@@ -8,41 +8,16 @@ import Sdata from './Sdata'
 ReactDOM.render(
     <>
         <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
-
-        {/* Custom attribute = Props */}
+        {Sdata.map((val) => {
+    return (
         <Card
-            imgsrc={Sdata[0].imgsrc}
-            title={Sdata[0].title}
-            sname={Sdata[0].sname}
-            link={Sdata[0].link}
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
         />
+    );
+})}
 
-        <Card
-            imgsrc={Sdata[1].imgsrc}
-            title={Sdata[1].title}
-            sname={Sdata[1].sname}
-            link={Sdata[1].link}
-        />
-
-        <Card
-            imgsrc={Sdata[2].imgsrc}
-            title={Sdata[2].title}
-            sname={Sdata[2].sname}
-            link={Sdata[2].link}
-        />
-
-        <Card
-            imgsrc={Sdata[3].imgsrc}
-            title={Sdata[3].title}
-            sname={Sdata[3].sname}
-            link={Sdata[3].link}
-        />
-
-        <Card
-            imgsrc={Sdata[4].imgsrc}
-            title={Sdata[4].title}
-            sname={Sdata[4].sname}
-            link={Sdata[4].link}
-        />
     </>
     , document.getElementById('root'));
