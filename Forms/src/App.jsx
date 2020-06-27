e129f0225cf7da4683612717434abf065795b1b6
jewells <jewellsjoshi437@gmail.com>
2020-06-27 20:06:20 +0530

    Login Forms (controlled) ->Hooks

--- Forms/src/App.jsx
+++ Forms/src/App.jsx
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 
 const App = () => {
     const [name, setName] = useState('');
-    const [fullName, setFullName] = useState();
+    const [pass, setPass] = useState('');
 
-    const inputEvent = (event) => {
+    const [fullName, setFullName] = useState('');
+    const [Password, setPassword] = useState('');
+
+    const inputName = (event) => {
         // console.log('words ', event.target.value);
         setName(event.target.value);
+    }
+
+    const inputPass = (event) => {
+        setPass(event.target.value);
     }
 
     const onSubmit = (event) => {
         event.preventDefault();
         setFullName(name);
+        setPassword(pass);
     }
 
     return (
@@ -19,8 +27,9 @@ const App = () => {
             <div className="main_div">
                 <form onSubmit={onSubmit}>
                     <div>
-                        <h1>Hello {fullName}</h1>
-                        <input type="text" placeholder="Enter Your Name" onChange={inputEvent} value={name} />
+                        <h1>Hello {fullName} {Password}</h1>
+                        <input type="text" placeholder="Enter Your Name" onChange={inputName} value={name} />
+                        <input type="password" placeholder="Enter Your Password" onChange={inputPass} value={pass} />
                         <button type="submit">Submit</button>
                     </div>
                 </form>
