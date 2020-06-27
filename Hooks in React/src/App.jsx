2db3da350e5bbfba6f4c3c957d06bc19214e059a
jewells <jewellsjoshi437@gmail.com>
2020-06-27 18:04:04 +0530

    Digital Clock-> Hooks

--- Hooks in React/src/App.jsx
+++ Hooks in React/src/App.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    // useState is called a Hook
-    const state = useState();
-    // console.log(state);
-
-
-    // useState(initial value) is passing to count and setCount is a fun that will updating and pass it to count
-    
-    let newTime = new Date().toLocaleTimeString();
-    const [ctime, setCtime] = useState(newTime);
-
+    let time = new Date().toLocaleTimeString();
+    const [ctime, setCtime] = useState(time);
 
     const UpdateTime = () => {
-        newTime = new Date().toLocaleTimeString();
-        setCtime(newTime);
-    }
+        time = new Date().toLocaleTimeString();
+        setCtime(time);
+    };
+
+    setInterval(UpdateTime, 1000);
+
     return (
         <>
             <h1>{ctime}</h1>
-            <button onClick={UpdateTime}>Get Time</button>
         </>
     );
 }
