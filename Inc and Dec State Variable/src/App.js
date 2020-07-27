9d911e5805557d9300425c68c6d33d2a5f4d23f6
jewells <jewellsjoshi437@gmail.com>
2020-07-27 12:00:50 +0530

    Inc and Dec State Variable

---
+++ Inc and Dec State Variable/src/App.js
@@ -0,0 +1,14 @@
+import React,{Fragment} from 'react';
+import ToDoLists from './ToDoLists';
+
+const App = () => {
+
+  
+    return (
+        <Fragment>
+        <ToDoLists/>
+        </Fragment>
+    );
+};
+
+export default App;
