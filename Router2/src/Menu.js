9f62fd060bed6c7bef5998b148995487482429a1
jewells <jewellsjoshi437@gmail.com>
2020-07-30 17:00:59 +0530

    React router (component vs render)

---
+++ Router2/src/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  NavLink } from 'react-router-dom';
+import './Menu.css'
+
+const Menu = () => {
+  return (
+    <div className="menu_style">
+      <NavLink exact activeClassName="active_class" to="/about">About </NavLink>
+      <NavLink exact activeClassName="active_class" to="/service">service </NavLink>
+      <NavLink exact activeClassName="active_class" to="/contact">Contact </NavLink>
+    </div>
+  );
+};
+
+export default Menu;
